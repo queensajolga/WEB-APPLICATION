e73c784c05905b22fc438275c5adce063ed07dea
queensajolga <queenanns.sajolga@gmail.com>
2025-04-06 00:27:43 +0800

    added login page

--- src/main.js
+++ src/main.js
@@ -7,12 +7,24 @@ import * as bootstrap from 'bootstrap'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
