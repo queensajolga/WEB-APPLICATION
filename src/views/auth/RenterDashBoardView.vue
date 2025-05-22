777611313ad1574d09d4d56eb422eb8d88e5358f
queensajolga <queenanns.sajolga@gmail.com>
2025-05-22 21:54:41 +0800

    remove bootstrap

--- src/views/auth/RenterDashBoardView.vue
+++ src/views/auth/RenterDashBoardView.vue
@@ -70,7 +70,7 @@ function navigateTo(path) {
       <v-container fluid class="py-6">
         <v-row justify="center" align="center" class="mb-6">
           <v-col cols="auto" class="d-flex align-center">
-            <v-img src="/public/favicon.ico" width="160" height="160" contain />
+            <v-img src="/favicon.ico" width="160" height="160" contain />
             <v-sheet class="nav-container px-6 py-2 ml-4" color="white">
               <v-row align="center" no-gutters>
                 <v-btn text class="nav-btn">Home</v-btn>
